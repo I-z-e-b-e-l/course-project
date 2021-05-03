3575ce488189743c3803b833811bfd59dd2d1e63
izebel <izebel@rocketmail.com>
2021-05-03 10:18:04 -0400

    actually homework-10

--- app.js
+++ app.js
@@ -10,9 +10,21 @@ app.get('/', (request, response) => {
     response.send(`This route points to the homepage`);
 })
 
+app.get('/about', (request, response) => {
+    response.send(`This route points to the About page`);
+})
 
+app.get('/login', (request, response) => {
+    response.send(`This route points to the Login page`);
+})
 
+app.get('/admin-console', (request, response) => {
+    response.send(`This route points to the Admin Console page`);
+})
 
+app.get('/admin-console/create-book', (request, response) => {
+    response.send(`This route points to the Create page`);
+})
 
 
 
